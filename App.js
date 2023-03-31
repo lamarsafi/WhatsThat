99de571d4e94b7ee2551b6a7ee14391c456820b9
lamars7 <lamarsafi7@gmail.com>
2023-03-31 21:14:51 +0100

    Fixing error

--- App.js
+++ App.js
@@ -1,25 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { StyleSheet, View, ImageBackground } from 'react-native';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
+import Home from './components/Home'
+import {Profile} from './components/Profile';
+import SecondLogin from './components/Login'
+import SecondRegister from './components/Register';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator, useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { registerAsset } from 'react-native-web/dist/cjs/modules/AssetRegistry';
+import Screens from './components/Screens';
+
+
+const AuthStack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+
+
 
 export default function App() {
-  const [screen, setScreen] = useState('login');
+ 
+  
 
-  const handleFormSwitch = (screenName) => {
-    setScreen(screenName);
-  };
 
   return (
     <ImageBackground
-    source={require('./images/bg4.jpg')}
-    style={styles.container}
-  >
-      <View>
-        
-        {screen === 'login' && <Login onFormSwitch={handleFormSwitch} />}
-        {screen === 'register' && <Register onFormSwitch={handleFormSwitch} />}
-      </View>
+      source={require('./images/bg4.jpg')}
+      style={styles.container}
+    >
+      <NavigationContainer>
+        <AuthStack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}>
+          
+          <AuthStack.Screen name='Screen' component={Screens} />
+          <AuthStack.Screen name='Home' component={Home} />
+          <AuthStack.Screen name='LoginScreen' component={SecondLogin} />
+          <AuthStack.Screen name='Register' component={SecondRegister} />
+          <AuthStack.Screen name='Profile' component={Profile} />
+
+        </AuthStack.Navigator>
+       {/*  <Tabs.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Tabs.Screen name='Home' component={HomeStackScreen} />
+          <Tabs.Screen name='Profile' component={ProfileStackScreen} />
+          
+        </Tabs.Navigator> */}
+      </NavigationContainer>
     </ImageBackground>
   );
 }
